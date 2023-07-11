468ebb69ef565868234c1b9730ef9d5af847f8fc
Rohan Godha <yoyorodha@gmail.com>
2023-07-11 21:37:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-const http = require("http");
+const http= require("http");
+//package name and importing of it
 
 const port = 8081;
+//localport number
 
 http
-    .createServer((require, response) => { 
-        response.writeHead(200,{'Content-Type': 'text/html'});
-        response.write("<h1>Hello, This is Rohan Godha's Server</h1>");
-        response.end();
-    })
-    .listen(port, () => {
-        console.log(`Nodejs server started on port number ${port}`);
-=======
-const http = require("http");
-
-const port = 8081;
-
-http
-    .createServer((require, response) => { 
-        response.writeHead(200,{'Content-Type': 'text/html'});
-        response.write("<h1>Hello, This is Rohan Godha's Server</h1>");
-        response.end();
-    })
-    .listen(port, () => {
-        console.log(`Nodejs server started on port number ${port}`);
->>>>>>> a50401b92cf3f70f575f30ebd806cb9262b2a98b
-    });
+.createServer((request, response)=>{
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write("<h1>Hi! Rohan</h1>");
+    response.end();    
+})
+.listen(port,()=>{
+    console.log(`Node started at ${port}`);
+}); 
